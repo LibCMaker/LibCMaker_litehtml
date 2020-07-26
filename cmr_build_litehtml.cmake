49dabbb132a842e147419689a28ec9314504dcc8
NikitaFeodonit <nfeodonit@yandex.com>
2020-07-26 15:50:29 +0300

    Use CACHE vars in cmr_build_*.cmake

--- cmr_build_litehtml.cmake
+++ cmr_build_litehtml.cmake
@@ -29,9 +29,9 @@
 # Lib's name, version, paths
 #-----------------------------------------------------------------------
 
-set(LITEHTML_lib_NAME      "litehtml")
-set(LITEHTML_lib_VERSION   "0.5.20200407")
-set(LITEHTML_lib_DIR       "${CMAKE_CURRENT_LIST_DIR}")
+set(LITEHTML_lib_NAME "litehtml")
+set(LITEHTML_lib_VERSION "0.5.20200407" CACHE STRING "LITEHTML_lib_VERSION")
+set(LITEHTML_lib_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE PATH "LITEHTML_lib_DIR")
 
 # To use our Find<LibName>.cmake.
 list(APPEND CMAKE_MODULE_PATH "${LITEHTML_lib_DIR}/cmake/modules")
@@ -41,7 +41,7 @@ list(APPEND CMAKE_MODULE_PATH "${LITEHTML_lib_DIR}/cmake/modules")
 # LibCMaker_<LibName> specific vars and options
 #-----------------------------------------------------------------------
 
-set(COPY_LITEHTML_CMAKE_BUILD_SCRIPTS ON)
+option(COPY_LITEHTML_CMAKE_BUILD_SCRIPTS "COPY_LITEHTML_CMAKE_BUILD_SCRIPTS" ON)
 
 
 #-----------------------------------------------------------------------
